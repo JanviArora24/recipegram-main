8f8c6c6ba11c11b9702e6ad462084e969fdbc336
JanviArora24 <janvi.jiyaarora@gmail.com>
2025-07-02 20:34:18 +0530

    �️ Set up React Router with route pages and Layout wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import TopNavbar from "./components/TopNavbar";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import AddRecipe from "./pages/AddRecipe";
+import Planner from "./pages/Planner";
+import Profile from "./pages/Profile";
+import Saved from "./pages/Saved";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <div>
-      <TopNavbar />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="feed" element={<Home />} />
+        <Route path="add-recipe" element={<AddRecipe />} />
+        <Route path="planner" element={<Planner />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="saved" element={<Saved />} />
+        <Route path="/settings" element={<Settings />} />
+      </Route>
+    </Routes>
   );
 }
 
